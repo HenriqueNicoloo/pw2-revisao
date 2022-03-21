0b153cacb4356122094d57fe3034b184c9946d6f
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test.com>
2022-03-21 11:11:37 -0300

    Desenvolvimento do exercicio 4

---
+++ src/ExercicioQuatro.java
@@ -0,0 +1,14 @@
+
+public class ExercicioQuatro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 4;
+		
+		if(numero > 0 && numero % 2 ==0 || numero %3 ==0 && numero != 15)
+			System.out.println("numero da sorte");
+			
+
+	}
+
+}

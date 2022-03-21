baf8e38c6a70d694cd316a2d3d4227635c8e4180
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test.com>
2022-03-21 10:45:22 -0300

    criação do projeto

---
+++ src/ExercicioDois.java
@@ -0,0 +1,17 @@
+
+public class ExercicioDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numeroA = 30;
+		int numeroB = 45;
+		int numeroC = numeroA;
+		numeroA = numeroB;
+		numeroB = numeroC;
+		System.out.println("Valor de A -->" + numeroA);
+		System.out.println("Valor de B -->" + numeroB);
+		System.out.println("Valor de C -->" + numeroC);
+	}
+
+}
---
+++ src/ExercicioUm.java
@@ -0,0 +1,12 @@
+
+public class ExercicioUm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = "Horácio";
+		String sobrenome = "Augusto da Silveira";
+		String idade = "30";
+		String resultado = nome + " " + sobrenome + " tem " + idade + " anos";
+		System.out.println(resultado);
+	}
+}

d4b873145976038efb3049ec2a3da669ce25e26a
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test.com>
2022-03-21 10:59:31 -0300

    Desenvolvimento do exercicio 3

---
+++ src/ExercicioTres.java
@@ -0,0 +1,17 @@
+
+public class ExercicioTres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 5;
+		
+		if(numero > 0)
+			System.out.println("1");
+		else if (numero == 0)
+			System.out.println("0");
+		else
+			System.out.println("-1");
+	}
+
+}

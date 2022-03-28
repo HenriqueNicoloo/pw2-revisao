7a0f555963179b580c606c94c0a01ed62599dc35
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test.com>
2022-03-28 10:36:34 -0300

    exercicio cinco

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,19 @@
+
+public class ExercicioCinco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double trabalhoDeHistoria = 8.0;
+		double trabalhoDeMatematica = 7.0;
+		double trabalhoDeCiencia = 10;
+		double trabalhoDeGeografia = 9.5;
+		double media;
+		
+		media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia) / 4;
+		
+		System.out.println("Sua média é " + media);
+		
+	}
+
+}
